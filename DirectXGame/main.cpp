da5cc14c770e6b86c77849ad09772163a8d2cac2
SR0204 <k023g0023@g.neec.ac.jp>
2025-02-03 14:26:51 +0900

    完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"TD2");
+	win->CreateGameWindow(L"ネビュラクラッシュ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
