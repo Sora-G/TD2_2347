db9a8962ae1d404d7f6d196a67d3e7a731425f72
SR0204 <k023g0023@g.neec.ac.jp>
2025-02-07 10:22:01 +0900

    タイトルバーを直した

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -45,7 +45,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"ネビュラクラッシュ");
+	win->CreateGameWindow(L"2347_ネビュラクラッシュ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
